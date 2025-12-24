ef7056b58bc7786528366fc0733ec70a88b2323a
Deepak <ds9523717267@gmail.com>
2025-12-24 22:32:45 +0530

    learing pyhton with Hitesh sir and caht gpt

---
+++ First/first.py
@@ -0,0 +1,18 @@
+# // For printing any thing in python 
+
+# // we are importin name and function from annother file and using it 
+# when we are importing any function or any data from another file then __ pycahe__ folder is created in same directry 
+# these folder caontains the compiled version of the imported file its basicaly is bytecode of that file 
+
+from second import name as person_name 
+print("the name is :", person_name)
+
+# for loop in python 
+
+name = "Deepak Kumar Singh"
+
+for char in name :
+    print(char)
+
+# in python indetion is very important it is used to define the bloack of code 
+# for example in above for loop the block of code is defined by the indention after for loop 
---
+++ First/second.py
@@ -0,0 +1,5 @@
+print("hellow in sencode file")
+def add(n) :
+    print("the number is :", n)
+
+name = "deepak Kumar Singh"
